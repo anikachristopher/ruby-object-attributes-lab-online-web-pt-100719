263b9dff12efe79967f6c383a9a7d26b26fd659d
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-25 18:52:52 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def initialize(name)
+    @this_dogs_name = name
+  end
+  
+  def name=(this_dogs_name)
+    @this_dogs_name = name 
+  end
+  
+  def name
+    "#{name}"
+  end
+  
+end  
+    
